22fe840fa05fec46207980bf57bc2e1c657d4a7e
Hebert Hendrik <hebert.hendrik@gmail.com>
2018-05-23 00:31:52 +0700

    update link dashboard

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -15,7 +15,7 @@
 			<nav id="menu" class="nav-main" role="navigation">
 				<ul class="nav nav-main">
 					<li class="nav-active">
-						<a href="#">
+						<a href="{{route('settings.dashboard.index')}}">
 							<i class="fa fa-home" aria-hidden="true"></i>
 							<span>Dashboard</span>
 						</a>
