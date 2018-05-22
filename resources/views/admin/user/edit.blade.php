8e51c93dd7b8d589cdbe3473481fd5c1519893d1
Hebert Hendrik <hebert.hendrik@gmail.com>
2018-05-23 00:22:50 +0700

    master data user, beres

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,27 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="content-header">
-    <h1>User</h1>
+<header class="page-header">
+	<h2>User</h2>
 
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
+	<div class="right-wrapper pull-right">
+		<ol class="breadcrumbs">
+			<li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
         <li><a href="{{route('settings.user.index')}}">User</a></li>
-        <li class="active">Edit</li>
-    </ol>
-</section>
+			<li><span>Edit</span></li>
+		</ol>
 
-<section class="content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Edit Data</h3>
-                </div>
-                {!! Former::populate($user) !!}
-                {!! Former::open(route('settings.user.update', $user->id))->method('put') !!}
-                <div class="box-body">
-                    {!! Former::text('name')->required() !!}
+		<a class="sidebar-right-toggle"><i class="fa fa-chevron-left"></i></a>
+	</div>
+</header>
+
+
+
+
+<!-- start: page -->
+<div class="row">
+	<div class="col-lg-12">
+		
+		{!! Former::populate($user) !!}
+		{!! Former::open(route('settings.user.update', $user->id))->method('put') !!}
+			<section class="panel">
+				<div class="panel-body">
+					
+					<div class="form-group">
+						<div class="col-md-12 text-right">
+							<a href="{{route('settings.user.index')}}"><button type="button" class="btn btn-default">Batal</button></a>
+							<button type="submit" class="btn btn-primary">Simpan</button>
+						</div>
+					</div>
+	
+				</div>
+			</section>
+			
+			<section class="panel">
+				<header class="panel-heading">
+					<div class="panel-actions">
+						<a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+						<a href="#" class="panel-action panel-action-dismiss" data-panel-dismiss></a>
+					</div>
+	
+					<h2 class="panel-title">Edit Data</h2>
+				</header>
+				<div class="panel-body">
+	
+					{!! Former::text('name')->required() !!}
                     {!! Former::text('email')->required() !!}
                     {!! Former::password('password')->required() !!}
                     {!! Former::select('roles')->options([
@@ -34,14 +61,14 @@
                     ->class('form-control select2')
                     ->options($filters['id_rs'])
                     ->required() !!}
-                </div>
-                <div class="box-footer">
-                    <button type="submit" class="btn btn-success">Simpan</button>
-                    <a href="{{route('settings.user.index')}}" class="btn btn-default">Batal</a>
-                </div>
-                {!! Former::close() !!}
-            </div>
-        </div>
-    </div>
-</section>
+                    
+				</div>
+			</section>
+		{!! Former::close() !!}
+		
+		
+
+	</div>
+</div>
+<!-- end: page -->
 @endsection
