c5424e7e9def864ef168af3d192c9620cc73d467
Hebert Hendrik <hebert.hendrik@gmail.com>
2018-05-22 19:40:53 +0700

    master data ruang beres

--- resources/views/admin/ruang/edit.blade.php
+++ resources/views/admin/ruang/edit.blade.php
@@ -1,27 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="content-header">
-    <h1>Ruang</h1>
 
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
-        <li><a href="{{route('settings.ruang.index')}}">Ruang</a></li>
-        <li class="active">Edit</li>
-    </ol>
-</section>
+<header class="page-header">
+	<h2>Ruang</h2>
 
-<section class="content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Edit Data</h3>
-                </div>
-                {!! Former::populate($ruang) !!}
-                {!! Former::open(route('settings.ruang.update', $ruang->id))->method('put') !!}
-                <div class="box-body">
-                    {!! Former::text('nama')->label('Nama Ruang')->required() !!}
+	<div class="right-wrapper pull-right">
+		<ol class="breadcrumbs">
+			<li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
+			<li><a href="{{route('settings.ruang.index')}}">Ruang</a></li>
+			<li><span>Edit</span></li>
+		</ol>
+
+		<a class="sidebar-right-toggle"><i class="fa fa-chevron-left"></i></a>
+	</div>
+</header>
+
+
+<!-- start: page -->
+<div class="row">
+	<div class="col-lg-12">
+		
+		{!! Former::populate($ruang) !!}
+		{!! Former::open(route('settings.ruang.update', $ruang->id))->method('put') !!}
+			<section class="panel">
+				<div class="panel-body">
+					
+					<div class="form-group">
+						<div class="col-md-12 text-right">
+							<a href="{{route('settings.ruang.index')}}"><button type="button" class="btn btn-default">Batal</button></a>
+							<button type="submit" class="btn btn-primary">Simpan</button>
+						</div>
+					</div>
+	
+				</div>
+			</section>
+			
+			<section class="panel">
+				<header class="panel-heading">
+					<div class="panel-actions">
+						<a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+						<a href="#" class="panel-action panel-action-dismiss" data-panel-dismiss></a>
+					</div>
+	
+					<h2 class="panel-title">Edit Data</h2>
+				</header>
+				<div class="panel-body">
+	
+					<style>
+						.required{
+							display:inherit;
+						}
+						label{
+							color:#777;
+							font-size:13px;
+						}
+						label sup{
+							color:red;
+							font-size:13px;
+							padding-left:5px;
+						}
+						select, .select2-selection__rendered, input{
+							font-size:13px!important;
+							font-weight:normal!important;
+						}
+						
+						.select2-selection{
+							height:35px!important;
+						}
+					</style>
+					
+					{!! Former::text('nama')->label('Nama Ruang')->required() !!}
                     {!! Former::select('status')->options([
                     '1' => 'Aktif',
                     '0' => 'Tidak Aktif'
@@ -31,14 +80,19 @@
                     ->class('form-control select2')
                     ->options($filters['id_rs'])
                     ->required() !!}
-                </div>
-                <div class="box-footer">
-                    <button type="submit" class="btn btn-success">Simpan</button>
-                    <a href="{{route('settings.ruang.index')}}" class="btn btn-default">Batal</a>
-                </div>
-                {!! Former::close() !!}
-            </div>
-        </div>
-    </div>
-</section>
+                    
+                    
+                    
+					
+                    
+	
+				</div>
+			</section>
+		{!! Former::close() !!}
+		
+		
+
+	</div>
+</div>
+<!-- end: page -->
 @endsection
