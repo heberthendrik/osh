de065d652358e94394d50c4040073d35a8c25b3e
Hebert Hendrik <hebert.hendrik@gmail.com>
2018-05-22 21:27:52 +0700

    master data barang, selesai

--- resources/views/admin/barang/create.blade.php
+++ resources/views/admin/barang/create.blade.php
@@ -1,26 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="content-header">
-    <h1>Barang</h1>
+<header class="page-header">
+	<h2>Barang</h2>
 
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
+	<div class="right-wrapper pull-right">
+		<ol class="breadcrumbs">
+			<li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
         <li><a href="{{route('settings.barang.index')}}">Barang</a></li>
-        <li class="active">Tambah</li>
-    </ol>
-</section>
+			<li><span>Tambah</span></li>
+		</ol>
 
-<section class="content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Input Data</h3>
-                </div>
-                {!! Former::open(route('settings.barang.store'))->method('post') !!}
-                <div class="box-body">
-                    {!! Former::text('name')->label('Nama Barang')->required() !!}
+		<a class="sidebar-right-toggle"><i class="fa fa-chevron-left"></i></a>
+	</div>
+</header>
+
+<!-- start: page -->
+<div class="row">
+	<div class="col-lg-12">
+		
+		{!! Former::open(route('settings.barang.store'))->method('post') !!}
+			<section class="panel">
+				<div class="panel-body">
+					
+					<div class="form-group">
+						<div class="col-md-12 text-right">
+							<a href="{{route('settings.barang.index')}}"><button type="button" class="btn btn-default">Batal</button></a>
+							<button type="submit" class="btn btn-primary">Simpan</button>
+						</div>
+					</div>
+	
+				</div>
+			</section>
+			
+			<section class="panel">
+				<header class="panel-heading">
+					<div class="panel-actions">
+						<a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+						<a href="#" class="panel-action panel-action-dismiss" data-panel-dismiss></a>
+					</div>
+	
+					<h2 class="panel-title">Input Data</h2>
+				</header>
+				<div class="panel-body">
+	
+					{!! Former::text('name')->label('Nama Barang')->required() !!}
                     {!! Former::select('id_satuan')
                     ->label('Satuan')
                     ->fromQuery(App\Models\Satuan::where('status', '1')->get(), 'nm_satuan', 'id')->required() !!}
@@ -51,14 +75,18 @@
                     ->class('form-control select2')
                     ->options($filters['id_rs'])
                     ->required() !!}
-                </div>
-                <div class="box-footer">
-                    <button type="submit" class="btn btn-success">Simpan</button>
-                    <a href="{{route('settings.barang.index')}}" class="btn btn-default">Batal</a>
-                </div>
-                {!! Former::close() !!}
-            </div>
-        </div>
-    </div>
-</section>
+    
+				</div>
+			</section>
+		{!! Former::close() !!}
+		
+		
+
+	</div>
+</div>
+<!-- end: page -->
+
+
+
+
 @endsection
--- resources/views/admin/barang/show.blade.php
+++ resources/views/admin/barang/show.blade.php
@@ -1,100 +1,115 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="content-header">
-    <h1>Barang</h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
-        <li><a href="{{route('settings.barang.index')}}">Barang</a></li>
-        <li class="active">Detil</li>
-    </ol>
-</section>
+<header class="page-header">
+	<h2>Barang</h2>
 
-<section class="invoice">
-    <div class="row">
-        <div class="col-xs-12">
-            <h2 class="page-header">
-                {{$barang->name}}
-            </h2>
-        </div>
-    </div>
-    @if($barang->count())
-    <div class="row">
-        <div class="col-md-12">
-            <table class="table table-striped table-responsive">
-                <tr>
-                    <th>Nama Barang</th>
-                    <td>:</td>
-                    <td>{{$barang->name}}</td>
-                    <th>Principal</th>
-                    <td>:</td>
-                    <td>{{$barang->id_principal}}</td>
-                </tr>
-                <tr>
-                    <th>Satuan</th>
-                    <td>:</td>
-                    <td>{{$barang->id_satuan}}</td>
-                    <th>Harga Perolehan</th>
-                    <td>:</td>
-                    <td align="right">{{number_format($barang->hrg_perolehan, 0, ',', '.')}}</td>
-                </tr>
-                <tr>
-                    <th>Katalog</th>
-                    <td>:</td>
-                    <td>{{$barang->katalog}}</td>
-                    <th>Harga Jual</th>
-                    <td>:</td>
-                    <td align="right">{{number_format($barang->hrg_jual, 0, ',', '.')}}</td>
-                </tr>
-                <tr>
-                    <th>Kategori</th>
-                    <td>:</td>
-                    <td>@if(isset($barang->id_kategori))
-                        {{$barang->kategori->nama}}
-                        @endif
-                    </td>
-                    <th>Status</th>
-                    <td>:</td>
-                    <td>{{$barang->status}}</td>
-                </tr>
-                <tr>
-                    <th>Supplier</th>
-                    <td>:</td>
-                    <td>{{$barang->id_supplier}}</td>
-                    <th>Komputer</th>
-                    <td>:</td>
-                    <td>{{$barang->komputer}}</td>
-                </tr>
-                <tr>
-                    <th>Tanggal Masuk</th>
-                    <td>:</td>
-                    <td>{{$barang->tgl_masuk}}</td>
-                    <th>User</th>
-                    <td>:</td>
-                    <td>{{$barang->user}}</td>
-                </tr>
-                <tr>
-                    <th>Merk</th>
-                    <td>:</td>
-                    <td>{{$barang->id_merk}}</td>
-                    <th>Tanggal Entri</th>
-                    <td>:</td>
-                    <td>{{$barang->tgl_entri}}</td>
-                </tr>
-                <tr>
-                    <th>Tipe</th>
-                    <td>:</td>
-                    <td>{{$barang->tipe}}</td>
-                    <th>Diskon</th>
-                    <td>:</td>
-                    <td align="right">{{number_format($barang->diskonv, 0, ',', '.')}}</td>
-                </tr>
-            </table>
-        </div>
-    </div>
-    @else
-    <i class="fa fa-fw fa-info-circle text-primary"></i> Data Tidak Ditemukan
-    @endif
-</section>
-<div class="clearfix"></div>
+	<div class="right-wrapper pull-right">
+		<ol class="breadcrumbs">
+			<li><a href="#"><i class="fa fa-gears"></i> Settings</a></li>
+        <li><a href="{{route('settings.barang.index')}}">Barang</a></li>
+			<li><span>Detil</span></li>
+		</ol>
+
+		<a class="sidebar-right-toggle"><i class="fa fa-chevron-left"></i></a>
+	</div>
+</header>
+
+<div class="row">
+	<div class="col-lg-12">
+		<section class="panel">
+			<header class="panel-heading">
+				<div class="panel-actions">
+					<a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
+					<a href="#" class="panel-action panel-action-dismiss" data-panel-dismiss></a>
+				</div>
+
+				<h2 class="panel-title">{{$barang->name}}</h2>
+			</header>
+			<div class="panel-body">
+			
+			<section class="invoice">
+			    @if($barang->count())
+			    <div class="row">
+			        <div class="col-md-12">
+			            <table class="table table-striped table-responsive">
+			                <tr>
+			                    <th>Nama Barang</th>
+			                    <td>:</td>
+			                    <td>{{$barang->name}}</td>
+			                    <th>Principal</th>
+			                    <td>:</td>
+			                    <td>{{$barang->id_principal}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Satuan</th>
+			                    <td>:</td>
+			                    <td>{{$barang->id_satuan}}</td>
+			                    <th>Harga Perolehan</th>
+			                    <td>:</td>
+			                    <td align="right">{{number_format($barang->hrg_perolehan, 0, ',', '.')}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Katalog</th>
+			                    <td>:</td>
+			                    <td>{{$barang->katalog}}</td>
+			                    <th>Harga Jual</th>
+			                    <td>:</td>
+			                    <td align="right">{{number_format($barang->hrg_jual, 0, ',', '.')}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Kategori</th>
+			                    <td>:</td>
+			                    <td>@if(isset($barang->id_kategori))
+			                        {{$barang->kategori->nama}}
+			                        @endif
+			                    </td>
+			                    <th>Status</th>
+			                    <td>:</td>
+			                    <td>{{$barang->status}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Supplier</th>
+			                    <td>:</td>
+			                    <td>{{$barang->id_supplier}}</td>
+			                    <th>Komputer</th>
+			                    <td>:</td>
+			                    <td>{{$barang->komputer}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Tanggal Masuk</th>
+			                    <td>:</td>
+			                    <td>{{$barang->tgl_masuk}}</td>
+			                    <th>User</th>
+			                    <td>:</td>
+			                    <td>{{$barang->user}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Merk</th>
+			                    <td>:</td>
+			                    <td>{{$barang->id_merk}}</td>
+			                    <th>Tanggal Entri</th>
+			                    <td>:</td>
+			                    <td>{{$barang->tgl_entri}}</td>
+			                </tr>
+			                <tr>
+			                    <th>Tipe</th>
+			                    <td>:</td>
+			                    <td>{{$barang->tipe}}</td>
+			                    <th>Diskon</th>
+			                    <td>:</td>
+			                    <td align="right">{{number_format($barang->diskonv, 0, ',', '.')}}</td>
+			                </tr>
+			            </table>
+			        </div>
+			    </div>
+			    @else
+			    <i class="fa fa-fw fa-info-circle text-primary"></i> Data Tidak Ditemukan
+			    @endif
+			</section>
+			
+			</div>
+		</section>
+	</div>
+</div>
 @endsection
